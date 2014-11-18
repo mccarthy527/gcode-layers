9b496b69814d956dd2ae5c750a4d802d936008b5
mccarthy527 <mccarthy527@gmail.com>
2014-11-18 17:30:19 -0600

    code is written
    still need to test and fix bugs

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-//testing github, modifying file from home.
+"use strict"
 
 module.exports = lines2layers
 
 function lines2layers(lines) 
 {
-	console.log{"this is a test")
+	var layers = [];
+	
+	for(i=0; i<lines.lines.length; i++)
+	{
+		var currz = 0;
+		var z1,z2;
+		if(lines.lines[i] != undefined && lines.extruded[i] == 1) //check to see if this is an extrusion move
+		{
+			z0 = ((lines.lines[i])[0])[2] //z coordinate of startpoint of line segment
+			z1 = ((lines.lines[i])[1])[2] // z coordinate of endpoint
+			if(z0 != z1){new Error('extruded line segment not in the plane! assumption violated!')}
+			
+			
+			//see if we are starting a new layer
+			if(z0 != currz)
+			{
+				layers.push({startheight:currz, endheight:z0, lineindicies:[i]}) //create a new layer
+			}
+			else
+			{
+				layers[layers.length-1].lineindicies.push(i);	//add the line index into the current layer
+			}
+		}
+	}
 }
---
+++ test/test.js
@@ -0,0 +1,13 @@
+"use strict"
+var ll = require('../index.js')
+var fs = require("fs")
+
+//not sure how to do this properly???
+var gc = require('gcode-parser')//var gc = require("C:/Users/Brian/Documents/GitHub/gcode-parser/index.js")
+var sl = require("C:/Users/Brian/Documents/GitHub/gcode-lines/index.js")
+
+var data = fs.readFileSync("C:/Users/Brian/Documents/GitHub/gcode-lines/test/testbox.gcode")
+var fileContent = data.toString()
+var states = gc(fileContent)
+var lines = sl(states)
+var layers = ll(lines)
